a04f8ccc0df9daaa4a920eeeb80e6bdeeeb811dd
shubha0008 <shubharashmi101@gmail.com>
2020-10-07 12:54:07 +0530

    Updates

---
+++ precache-manifest.52c00acafd9c2881905d0a303601e537.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be6a6a9c497eafd119031e4e8d6a68e2",
+    "url": "/eventMgmt_frontend/index.html"
+  },
+  {
+    "revision": "171b9bf61c1d281d15a3",
+    "url": "/eventMgmt_frontend/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "1eadff4ab90471b68e60",
+    "url": "/eventMgmt_frontend/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "171b9bf61c1d281d15a3",
+    "url": "/eventMgmt_frontend/static/js/2.3c68dca1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/eventMgmt_frontend/static/js/2.3c68dca1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1eadff4ab90471b68e60",
+    "url": "/eventMgmt_frontend/static/js/main.16eccab1.chunk.js"
+  },
+  {
+    "revision": "ad1eb05fdd5dddf99a62",
+    "url": "/eventMgmt_frontend/static/js/runtime-main.edcedb1f.js"
+  }
+]);
